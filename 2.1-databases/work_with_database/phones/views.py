124948724b9165004bb0b2d94e89ba7fbf34996e
VitZur <131258965+VitZur@users.noreply.github.com>
2024-10-08 12:17:20 +0300

    Add files via upload

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -5,15 +5,14 @@ def show_product(request):
    
     sort_by = request.GET.get('sort', 'name')  
 
-    if sort_by == 'name':
-        phones = Phone.objects.all().order_by('name')
-    elif sort_by == 'price':
-        phones = Phone.objects.all().order_by('price')
-    elif sort_by == 'price_desc':
-        phones = Phone. objects.all().order_by('-price')
-    else:
-        phones = Phone.objects.all()
-        
+    sort_options = {
+        'name':'name',
+        'price':'price',
+        'price_desc':'-price'
+    }
+
+    phones = Phone.objects.all().order_by(sort_options.get(sort_by,'name'))
+    
     return render(request, 'catalog.html', {'phones':phones})
 
 def index(request):
